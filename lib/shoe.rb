3f01391fc7d90c2af56d47327f9e77ef4798da1b
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-07-22 19:11:36 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,42 +1,12 @@
 class Shoe
+attr_accessor :color, :size, :material, :condition
+
   def initialize(brand)
     @brand = brand
   end
 
   def brand
     @brand
-  end
-
-  def color=(color)
-    @color = color
-  end
-
-  def color
-    @color
-  end
-
-  def size=(size)
-    @size = size
-  end
-
-  def size
-    @size
-  end
-
-  def material=(mat)
-    @mat = mat
-  end
-
-  def material
-    @mat
-  end
-
-  def condition=(cond)
-    @cond = cond
-  end
-
-  def condition
-    @cond
   end
 
   def cobble
